d7f06265d79677beadce0cc1972796a9206c0c2a
Tanmay bhure <109850676+Tanmaybhure@users.noreply.github.com>
2024-02-25 17:58:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,269 @@
+const teampeople=[{
+    image:'photo/ADITYA SINGH.jpg',
+    name:'Aditya Singh',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/12.jpg',
+    name:'Aditya Singh',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Aditya_Lad.jpg',
+    name:'Aditya Lad',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Aishwarya_Pathak.jpg',
+    name:'Aishwarya Pathak',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Amogh_chandragiri.jpg',
+    name:'Amogh chandragiri',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Ankita Anarase.jpg',
+    name:'Ankita Anarase',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Atharva_Bardapurkar.jpg',
+    name:'Atharva Bardapurkar',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Atharva_Lende.jpg',
+    name:'Atharva Lende',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/copy of vaibhavi Pawar.jpg',
+    name:'Vaibhavi Pawar',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Disha_Parale.jpg',
+    name:'Disha Parale',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/DSCF8504.jpg',
+    name:'Atharva Bardapurkar',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Jayati_Wajire.jpg',
+    name:'Jayati Wajire',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Khushi_Thakare.jpg',
+    name:'Khushi Thakare',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Lubhavan_Burghate.jpg',
+    name:'Lubhavan Burghate',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Mahima_Nair.jpg',
+    name:'Mahima Nair',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Mithilesh_Rajput.jpg',
+    name:'Mithilesh Rajput',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Nikita_Desale.jpg',
+    name:'Nikita Desale',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Nitin_Pandita.jpg',
+    name:'Nitin Pandita',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Prajwal_Lonari.jpg',
+    name:'Prajwal Lonari',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Pranay_ambade.jpg',
+    name:'Pranav Ambade',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Prasad_Mangnale.jpg',
+    name:'Prasad Mangnale',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Prathamesh B.jpg',
+    name:'Prathamesh Bacchav',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Prathamesh Theurkar.jpg',
+    name:'Prathamesh Theurkar',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Priyanka_Nalla.jpg',
+    name:'Priyanka Nulla',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Ronak_Dagale.jpg',
+    name:'Ronak Dagale',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Sachin Girawale.jpg',
+    name:'Sachin Girawale',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Sakshi_kulkarni.jpg',
+    name:'Sakshi Kulkarni',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Shivraj_Lawhare.jpg',
+    name:'Shivraj Lawhare',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Siddheshwar_Rede.jpg',
+    name:'Siddheshwar Rede',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Swayam_Patil.jpg',
+    name:'Swayam Patil',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Tarun Rathod.jpg',
+    name:'Tarun Rathod',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Vaishnav_Raundhal.jpg',
+    name:'Vaishnav Raundhal',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Vallabh_Kulkarni.jpg',
+    name:'Vallabh Kulkarni',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Vedant_Patil.jpg',
+    name:'Vedant Patil',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Yash_Deshmane.jpg',
+    name:'Yash Deshmane',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+{
+    image:'photo/Sachin Girawale.jpg',
+    name:'Sachin Girawale',
+    github:'<i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>',
+    mail:'<i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>',
+    link:'<i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>'
+},
+];
+let teamHtml='';
+teampeople.forEach((person)=>{
+teamHtml+=`
+<div class="team-members">
+    <img src="${person.image}" alt="hello">
+    <h2>${person.name}</h2>
+    <i class="fa-brands fa-github fa-2xl  githubclass" style="color: #04070b;"></i>
+    <i class="fa-regular fa-envelope fa-2xl githubclass" style="color: #070709;"></i>
+    <i class="fa-brands fa-linkedin fa-2xl githubclass" style="color: #000000;"></i>
+</div> `;
+});
+
+console.log(teamHtml);
+
+document.querySelector('.team-js').innerHTML= teamHtml;
+
